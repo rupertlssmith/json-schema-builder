cafff616065f54ebd204476d960379c421d2ddda
Rupert Smith <rupert@thesett.com>
2017-11-21 11:52:06 +0000

    Sketch of decoding a string field within an object.

--- src/JsonSchema/Builder.elm
+++ src/JsonSchema/Builder.elm
@@ -24,11 +24,60 @@ type Builder result
     = Builder (() -> Decoder result)
 
 
-
--- type SchemaType
---     = ObjectBuilder (List JsonSchema.ObjectSchemaProperty -> Schema)
-
-
 object : (fields -> a) -> Builder (fields -> a)
 object ctr =
     Builder (always (Decode.succeed ctr))
+
+
+map2 :
+    (a -> b -> c)
+    -> Builder a
+    -> Builder b
+    -> Builder c
+map2 f (Builder decoderA) (Builder decoderB) =
+    let
+        joinedDecoder selectionSet =
+            Decode.map2 f (decoderA selectionSet) (decoderB selectionSet)
+    in
+        Builder joinedDecoder
+
+
+with :
+    Builder a
+    -> Builder (a -> b)
+    -> Builder b
+with selection objectSpec =
+    map2 (<|) objectSpec selection
+
+
+field :
+    String
+    -> Builder result
+    -> Builder result
+field name (Builder decoder) =
+    Builder
+        decoder
+
+
+string : Builder String
+string =
+    primitive Decode.string
+
+
+primitive : Decoder result -> Builder result
+primitive decoder =
+    Builder
+        (always decoder)
+
+
+
+---
+
+
+type alias Test =
+    { a : String }
+
+
+test =
+    object Test
+        |> with (field "a" string)
