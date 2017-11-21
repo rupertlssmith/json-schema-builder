685fdf3ff72ac507cabd98b5a034e82101a623ef
Rupert Smith <rupert@thesett.com>
2017-11-21 10:59:09 +0000

    Initial import of json schema builder.

---
+++ src/JsonSchema/Builder.elm
@@ -0,0 +1,12 @@
+module JsonSchema.Builder exposing (..)
+
+import JsonSchema exposing (Schema)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode
+
+
+type alias Builder a =
+    { schema : Schema
+    , enocoder : a -> Encode.Value
+    , decoder : Decoder
+    }
