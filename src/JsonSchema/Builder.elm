7492a7aa29b578f95cd2dfdab39c16c974969ef1
Rupert Smith <rupert@thesett.com>
2017-11-21 11:28:16 +0000

    Compiling with elm-make.

--- src/JsonSchema/Builder.elm
+++ src/JsonSchema/Builder.elm
@@ -1,12 +1,34 @@
-module JsonSchema.Builder exposing (..)
+module JsonSchema.Builder exposing (Result)
+
+{-| Module docs
+
+@docs Result
+
+-}
 
 import JsonSchema exposing (Schema)
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
 
 
-type alias Builder a =
-    { schema : Schema
-    , enocoder : a -> Encode.Value
-    , decoder : Decoder
+{-| The result.
+-}
+type alias Result result =
+    { --schema : List JsonSchema.ObjectSchemaProperty -> Schema
+      --, enocoder : a -> Encode.Value
+      decoder : Decoder result
     }
+
+
+type Builder result
+    = Builder (() -> Decoder result)
+
+
+
+-- type SchemaType
+--     = ObjectBuilder (List JsonSchema.ObjectSchemaProperty -> Schema)
+
+
+object : (fields -> a) -> Builder (fields -> a)
+object ctr =
+    Builder (always (Decode.succeed ctr))
