e513397edd30247ee3f6c9973fd89d9a6e1f928a
Rupert Smith <rupert@thesett.com>
2017-11-24 16:53:40 +0000

    Creating a test for the model builder.

---
+++ tests/Builder.elm
@@ -0,0 +1,49 @@
+module Builder exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer)
+import Test exposing (..)
+import JsonSchema.Builder exposing (..)
+import Json.Encode exposing (Value)
+import Result exposing (Result)
+
+
+type alias ObjectSimpleFields =
+    { a : String
+    , b : Int
+    , c : Float
+    , d : Bool
+    }
+
+
+objectSimpleFields =
+    { a = "test"
+    , b = 2
+    , c = 5.678
+    , d = True
+    }
+
+
+
+-- objectSimpleFieldsSpec =
+--     build
+--         (object ObjectSimpleFields
+--             |> with (field "a" .a string)
+--             |> with (field "b" .b integer)
+--             |> with (field "c" .c number)
+--             |> with (field "d" .d boolean)
+--         )
+--
+--
+-- testDecodeObjectSimpleFields : Test
+-- testDecodeObjectSimpleFields =
+--     test "An object with simple fields builds." <|
+--         \_ ->
+--             let
+--                 value =
+--                     objectSimpleFieldsSpec objectSimpleFields
+--
+--                 d =
+--                     Debug.log "test" value
+--             in
+--                 Expect.pass
