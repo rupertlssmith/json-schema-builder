efec81516bdf20e747fc26e3fa46817ad0011660
Rupert Smith <rupert@thesett.com>
2017-11-24 17:08:20 +0000

    Added nested object encoder example that shows how the encoder does not allow this yet.

--- tests/Encode.elm
+++ tests/Encode.elm
@@ -8,6 +8,10 @@ import Json.Encode exposing (Value)
 import Result exposing (Result)
 
 
+-- Basic types.
+-- Objects with simple fields.
+
+
 type alias ObjectSimpleFields =
     { a : String
     , b : Int
@@ -41,6 +45,50 @@ testDecodeObjectSimpleFields =
             let
                 value =
                     objectSimpleFieldsEncoder objectSimpleFields
+
+                d =
+                    Debug.log "test" value
+            in
+                Expect.pass
+
+
+
+-- Objects with optional fields defaulting to Nothing.
+-- Objects with nested objects.
+
+
+type alias ObjectOuter =
+    { inner : ObjectSimpleFields
+    }
+
+
+objectOuter =
+    "{ \"inner\" : { \"a\" : \"test\", \"b\" : 2, \"c\" : 5.678, \"d\" : true } }"
+
+
+objectOuterEncoder =
+    build
+        (object ObjectOuter
+            |> with
+                (field "inner"
+                    .inner
+                    (object ObjectSimpleFields
+                        |> with (field "a" .a string)
+                        |> with (field "b" .b integer)
+                        |> with (field "c" .c number)
+                        |> with (field "d" .d boolean)
+                    )
+                )
+        )
+
+
+testDecodeObjectOuter : Test
+testDecodeObjectOuter =
+    test "An object with an inner object decodes." <|
+        \_ ->
+            let
+                value =
+                    objectOuterEncoder objectOuter
 
                 d =
                     Debug.log "test" value
