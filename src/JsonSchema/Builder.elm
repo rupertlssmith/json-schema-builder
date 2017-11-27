4784398c07417b2e0f56ffd4af1b518b9ee30a94
Rupert Smith <rupert@thesett.com>
2017-11-27 16:01:51 +0000

    Tidying up and renamed Field to Struc.

--- src/JsonSchema/Builder.elm
+++ src/JsonSchema/Builder.elm
@@ -8,108 +8,84 @@ module JsonSchema.Builder
         , integer
         , number
         , boolean
-        , Object(..)
-        , Field(..)
+        , Struc(..)
         )
 
 {-| Module docs
 
-@docs Object, Field
+@docs Struc
 @docs string, integer, number, boolean
 @docs build, object, with, field
 
 -}
 
 
-type alias ObjectSimpleFields =
-    { a : String
-    , b : Int
-    , c : Float
-    , d : Bool
-    }
-
-
-objectSimpleFieldsSpec =
-    object ObjectSimpleFields
-        |> with (field "a" .a string)
-        |> with (field "b" .b integer)
-        |> with (field "c" .c number)
-        |> with (field "d" .d boolean)
-        |> build
-
-
-{-| Object specifications.
+{-| Struc specifications.
 -}
-type Object
-    = Nah
-
-
-{-| Field specifications.
--}
-type Field obj
-    = IntField (obj -> Int)
-    | StrField (obj -> String)
-    | NumField (obj -> Float)
-    | BoolField (obj -> Bool)
-    | Object (List ( String, Field obj ))
+type Struc obj
+    = IntStruc (obj -> Int)
+    | StrStruc (obj -> String)
+    | NumStruc (obj -> Float)
+    | BoolStruc (obj -> Bool)
+    | Object (List ( String, Struc obj ))
 
 
 {-| Runs the builder.
 -}
-build : List ( String, Field obj ) -> Field obj
+build : List ( String, Struc obj ) -> Struc obj
 build fieldSpecs =
     Object fieldSpecs
 
 
 {-| Builds an object.
 -}
-object : cons -> List ( String, Field obj )
+object : cons -> List ( String, Struc obj )
 object _ =
     []
 
 
 {-| Adds fields to an object.
 -}
-with : List ( String, Field obj ) -> List ( String, Field obj ) -> List ( String, Field obj )
+with : List ( String, Struc obj ) -> List ( String, Struc obj ) -> List ( String, Struc obj )
 with fieldEncoder remainderEncoder =
     List.append (fieldEncoder) (remainderEncoder)
 
 
 {-| Builds a field.
 -}
-field : String -> (obj -> field) -> ((obj -> field) -> Field obj) -> List ( String, Field obj )
+field : String -> (obj -> field) -> ((obj -> field) -> Struc obj) -> List ( String, Struc obj )
 field name lens encoder =
     encode name lens encoder |> List.singleton
 
 
-encode : String -> (obj -> field) -> ((obj -> field) -> Field obj) -> ( String, Field obj )
+encode : String -> (obj -> field) -> ((obj -> field) -> Struc obj) -> ( String, Struc obj )
 encode name lens encoder =
     ( name, encoder lens )
 
 
 {-| Builds an integer type.
 -}
-integer : (obj -> Int) -> Field obj
+integer : (obj -> Int) -> Struc obj
 integer =
-    IntField
+    IntStruc
 
 
 {-| Builds a string type.
 -}
-string : (obj -> String) -> Field obj
+string : (obj -> String) -> Struc obj
 string =
-    StrField
+    StrStruc
 
 
 {-| Builds a number (float) type.
 -}
-number : (obj -> Float) -> Field obj
+number : (obj -> Float) -> Struc obj
 number =
-    NumField
+    NumStruc
 
 
 {-| Builds a boolean type.
 -}
-boolean : (obj -> Bool) -> Field obj
+boolean : (obj -> Bool) -> Struc obj
 boolean =
-    BoolField
+    BoolStruc
